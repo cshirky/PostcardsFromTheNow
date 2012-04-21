b7c8290dd28e87356ef7007ba0dfed6087441200
luisaph <luisaph@gmail.com>
2012-04-21 18:19:07 -0400

    styled comments

--- postcards.rb
+++ postcards.rb
@@ -35,7 +35,7 @@ class MyUploader < CarrierWave::Uploader::Base
        %w(jpg jpeg gif png)
    end
     
-  process :resize_to_limit =>[600,600]  
+  process :resize_to_limit =>[800,600]  
       
    version :thumb do
           process :resize_to_fill => [300,200]
